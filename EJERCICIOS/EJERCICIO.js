090e83307fa75372aeb7a59a09111de586c8f31e
JuliethGiraldo <juliethgs0101@gmail.com>
2024-01-18 10:29:23 -0500

    Primer ejercicio

---
+++ EJERCICIOS/EJERCICIO.js
@@ -0,0 +1,20 @@
+/*
+alert ("¡Bienvenida y bievenido a nuestro sitio web!");
+
+let nombre = ('Luna');
+let edad = 25;
+let numeroDeVentas = 50;
+let saldoDisponible = 1000;
+
+alert ("¡Error! Completa todos los campos");
+
+let mensajeDeError = '¡Error! Completa todos los campos'
+alert(mensajeDeError);
+
+let Nombre = prompt ('Dijite su nombre');
+
+let Edad = prompt ('Dijite su edad');
+if (Edad >= 18) {
+    alert('¡Puede obtener su licencia de conducir!');
+}
+*/
---
+++ EJERCICIOS/app.js
@@ -0,0 +1,14 @@
+//variable
+let numeroSecreto = 4;
+let numeroUsuario = prompt("Elige un número entre 1 y 10:");
+
+console.log(numeroUsuario);
+/*Este codigo realiza la comparacion*/
+
+
+if (numeroUsuario == numeroSecreto) {
+    alert(`Adivinaste, el numero es: ${numeroUsuario}`);
+} else {
+    alert('Lo siento, no acertaste el numero');
+}
+
