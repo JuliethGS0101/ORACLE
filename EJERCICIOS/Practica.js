87f454b2373cd57bf832ba82a2b23668d6921592
JuliethGiraldo <juliethgs0101@gmail.com>
2024-01-20 19:15:51 -0500

    Cuarto ejercicio actualizado

---
+++ EJERCICIOS/Practica.js
@@ -0,0 +1,1 @@
+//8) Crea una variable "numero" y solicita un valor con prompt. Luego, verifica si es positivo, negativo o cero utilizando un if-else y muestra el mensaje correspondiente.
--- EJERCICIOS/app.js
+++ EJERCICIOS/app.js
@@ -1,21 +1,38 @@
 //variable
-let numeroSecreto = 6;
+let numeroSecreto = Math.floor(Math.random()*10)+ 1;
 let numeroUsuario = 0;
-while (numeroUsuario != numeroSecreto){
-    numeroUsuario = prompt("Elige un número entre 1 a 10:");
+let intentos = 1;
+//let palabraVeces = 'vez';
+let maximosIntentos = 3;
 
-    console.log(numeroUsuario);
-    /*Este codigo realiza la comparacion*/
+console.log(numeroSecreto);
 
+while (numeroUsuario != numeroSecreto) {
+    numeroUsuario = parseInt(prompt("Me indicas un número entre 1 al 10 por favor:"));
+
+    console.log(typeof(numeroUsuario));
     if (numeroUsuario == numeroSecreto) {
-        alert(`Adivinaste, el numero es: ${numeroUsuario}`);
+        //Acertamos, fue verdadera la condición
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces' }`);
     } else {
         if (numeroUsuario > numeroSecreto) {
-            alert('El numero secreto es menor');
+            alert('El número secreto es menor');
         } else {
-            alert('El numero secreto es mayor');
+            alert('El número secreto es mayor');
         }
-        //alert('Lo siento, no acertaste el numero');
+        //Incrementamos el contador cuando no acierta
+        //intentos = intentos + 1;
+        //intentos += 1;
+        intentos++;
+
+        //palabraVeces = 'veces';
+        if (intentos > maximosIntentos) {
+            alert(`Llegaste al número máximo de 
+            ${maximosIntentos} intentos`);
+            break;
+        }
+        //La condición no se cumplió
+        //alert('Lo siento, no acertaste el número');
     }
 }
 
